bd330f7e178c6dfe0adb1852b5e741008934cf80
Lucas <lucasmacielaguiar@gmail.com>
2020-05-11 20:55:13 -0300

    Add Request Permission on ActivityMain

--- SpotifyClone/app/src/main/java/com/spotifyclone/presentation/MainActivity.kt
+++ SpotifyClone/app/src/main/java/com/spotifyclone/presentation/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.spotifyclone.presentation
 
+import android.Manifest
+import android.content.ContentValues
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.content.PermissionChecker
 import com.spotifyclone.presentation.base.BaseActivity
 import com.spotifyclone.presentation.home.HomeActivity
 import com.spotifyclone.presentation.login.LoginActivity
@@ -20,10 +27,14 @@ class MainActivity : BaseActivity() {
         val userLogged = false
         val development = true
 
+        isStoragePermissionGranted()
+        if (!isStoragePermissionGranted()) {
+            // intent = activityRequestPermissions
+        }
+
         if (development) {
             intent = getActivityBeingTested()
-        }
-        else {
+        } else {
             intent = if (userLogged) {
                 Intent(context, HomeActivity::class.java)
             } else {
@@ -34,7 +45,33 @@ class MainActivity : BaseActivity() {
         context.startActivity(intent)
     }
 
-    fun getActivityBeingTested() : Intent {
+    private fun isStoragePermissionGranted(): Boolean {
+        if (Build.VERSION.SDK_INT >= 23) {
+
+            return if (PermissionChecker.checkSelfPermission(
+                    applicationContext,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PermissionChecker.PERMISSION_GRANTED
+            ) {
+                Log.v(ContentValues.TAG, "Permission is granted")
+                true
+            } else {
+
+                Log.v(ContentValues.TAG, "Permission is revoked")
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    1
+                )
+                false
+            }
+        } else { //permission is automatically granted on sdk<23 upon installation
+            Log.v(ContentValues.TAG, "Permission is granted")
+            return true
+        }
+    }
+
+    private fun getActivityBeingTested(): Intent {
         return Intent(context, LikedSongsActivity::class.java)
     }
 }
