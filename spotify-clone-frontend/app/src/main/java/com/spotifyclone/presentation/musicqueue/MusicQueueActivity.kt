7a336e959f0297451b2bf9a76d69f4d38c3917c9
Lucas <lucasmacielaguiar@gmail.com>
2020-06-27 18:19:26 -0300

    [#20] - Create Music Player Fragment XML

--- spotify-clone-frontend/app/src/main/java/com/spotifyclone/presentation/musicqueue/MusicQueueActivity.kt
+++ spotify-clone-frontend/app/src/main/java/com/spotifyclone/presentation/musicqueue/MusicQueueActivity.kt
@@ -15,8 +15,8 @@ import com.spotifyclone.tools.utils.ImageUtils
 import com.spotifyclone.tools.utils.TextUtils
 import kotlinx.android.synthetic.main.activity_music_queue.*
 import kotlinx.android.synthetic.main.activity_music_queue.view.*
-import kotlinx.android.synthetic.main.dialog_bottom.*
-import kotlinx.android.synthetic.main.dialog_bottom.view.*
+import kotlinx.android.synthetic.main.fragment_two_options.*
+import kotlinx.android.synthetic.main.fragment_two_options.view.*
 import kotlinx.android.synthetic.main.include_toolbar.*
 
 class MusicQueueActivity : BaseActivity(), MusicObserver {
@@ -97,8 +97,8 @@ class MusicQueueActivity : BaseActivity(), MusicObserver {
         val addButton = dialog.btnAdd
         val removeButton = dialog.btnRemove
 
-        addButton.text = getString(R.string.music_queue_dialog_add_music)
-        removeButton.text = getString(R.string.music_queue_dialog_remove_music)
+        addButton.text = getString(R.string.music_queue_fragment_add_music)
+        removeButton.text = getString(R.string.music_queue_fragment_remove_music)
 
         addButton.setOnClickListener {
             funcAddMusic.invoke()
