7925bae71bc0ab7100039f782ed06c273727b4fd
Lucas <lucasmacielaguiar@gmail.com>
2020-08-01 14:56:20 -0300

    Remove useless test playlists in Home fragment

--- spotify-clone-frontend/app/src/main/java/com/spotifyclone/data/model/Playlist.kt
+++ spotify-clone-frontend/app/src/main/java/com/spotifyclone/data/model/Playlist.kt
@@ -1,6 +1,6 @@
 package com.spotifyclone.data.model
 
-
 data class Playlist (
-    val title: String
+    val title: String,
+    val layoutFragment: String
 )
--- spotify-clone-frontend/app/src/main/java/com/spotifyclone/presentation/home/RecommendedPlaylistsViewModel.kt
+++ spotify-clone-frontend/app/src/main/java/com/spotifyclone/presentation/home/RecommendedPlaylistsViewModel.kt
@@ -1,35 +1,34 @@
 package com.spotifyclone.presentation.home
 
+import android.content.Context
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.spotifyclone.R
 import com.spotifyclone.data.model.Playlist
 import java.lang.IllegalArgumentException
 
-class RecommendedPlaylistsViewModel : ViewModel() {
+class RecommendedPlaylistsViewModel(val context: Context) : ViewModel() {
 
     val recommendedPlaylistsLiveData: MutableLiveData<List<Playlist>> = MutableLiveData()
 
     fun getRecommendedPlaylists() {
         val recommendedList = listOf(
-            Playlist("Local Songs"),
-            Playlist("Synthwave"),
-            Playlist("Wake radio"),
-            Playlist("Holographic"),
-            Playlist("Strange Encounters Radio Station B landscape"),
-            Playlist("Betters")
+            Playlist("Local Songs", context.getString(R.string.fragment_local_songs_title))
         )
 
         recommendedPlaylistsLiveData.value = recommendedList
     }
 
-    class ViewModelFactory : ViewModelProvider.Factory {
+    class ViewModelFactory(
+        val context: Context
+    ) : ViewModelProvider.Factory {
+        @Suppress("UNCHECKED_CAST")
         override fun <T : ViewModel?> create(modelClass: Class<T>): T {
             if (modelClass.isAssignableFrom(RecommendedPlaylistsViewModel::class.java)) {
-                return RecommendedPlaylistsViewModel() as T
+                return RecommendedPlaylistsViewModel(context) as T
             }
             throw IllegalArgumentException("Unknown ViewModel class")
         }
     }
-
 }
